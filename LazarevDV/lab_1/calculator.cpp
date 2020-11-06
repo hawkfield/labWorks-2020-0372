179e3c5abd323ef8c7b0ea57c4ab14affe2ad79a
hawkfield <71931558+hawkfield@users.noreply.github.com>
2020-11-06 22:05:27 +0600

    Update calculator.cpp

--- LazarevDV/lab_1/calculator.cpp
+++ LazarevDV/lab_1/calculator.cpp
@@ -6,28 +6,27 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    long double secondvalue, firstvalue;
+    long double secondvalue, firstvalue, result;
     char sign, check;
-    cout << "Добро пожаловать в калькулятор Дениса Лазарева!\n";   
-    do { 
-    cout << "Введите два числа и знак операции:\n";
-    cin >> firstvalue >> secondvalue >> sign;
-    switch (sign) {
-    case '+': cout << firstvalue << " + " << secondvalue << " = " << firstvalue + secondvalue << endl; break;
-    case '-': cout << firstvalue << " - " << secondvalue << " = " << firstvalue - secondvalue << endl; break;
-    case '*': cout << firstvalue << " * " << secondvalue << " = " << firstvalue * secondvalue << endl; break;
-    case '/': if (secondvalue != 0) {
-        cout << firstvalue << " / " << secondvalue << " = " << firstvalue / secondvalue << endl; break;
-    }
-            else
-    {
-        cout << "Вы попробовали поделить на ноль, попробуйте еще раз\n"; 
-        break;
-    }
-    default: cout << "Вы ввели неверный знак, попробуйте еще раз\n"; break; 
-    }
-    cout << "Нажмите Enter для продолжения или Esc для выхода из программы\n";
-    check = _getch();
+    cout << "Добро пожаловать в калькулятор Дениса Лазарева!\n";
+    do {
+        cout << "Введите два числа и знак операции:\n";
+        cin >> firstvalue >> secondvalue >> sign;
+        switch (sign) {
+        case '+': result = firstvalue + secondvalue; break;
+        case '-': result = firstvalue - secondvalue; break;
+        case '*': result = firstvalue * secondvalue; break;
+        case '/': if (secondvalue != 0) {
+            result = firstvalue / secondvalue; break;
+        }
+                else {
+            cout << "Вы попробовали поделить на ноль, попробуйте еще раз\n";
+            break;
+        }
+        default: cout << "Вы ввели неверный знак, попробуйте еще раз\n"; break;
+        }
+        cout << "Результат операции:" << result << "\n" << "Нажмите Enter для продолжения или Esc для выхода из программы\n";
+        check = _getch();
     } while (check != 27);
     return 0;
 }
